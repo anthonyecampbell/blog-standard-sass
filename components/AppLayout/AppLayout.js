ebf0d419621651c5119d73ae6ec5124ae9ac6086
Anthony Campbell <aecampbell12@gmail.com>
2023-06-09 16:18:21 -0500

    edit footer layout
    add styling for footer layout

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,4 +1,10 @@
+import Link from 'next/link';
+import {useUser} from '@auth0/nextjs-auth0/client';
+import Image from 'next/image';
+
 export const AppLayout = ({children}) => {
+    const {user} = useUser();
+
     return (
         <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
             <div className="flex flex-col text-white overflow-hidden">
@@ -10,8 +16,26 @@ export const AppLayout = ({children}) => {
                 <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
                     list of posts
                 </div>
-                <div className="bg-cyan-800">
-                    user information - logout button
+                <div className="bg-cyan-800 flex items-center gap-2 border-t border-t-black/50 h-20 px-2">
+                    {!!user ? (
+                        <>
+                            <div className="min-w-[50px]">
+                                <Image 
+                                    src={user.picture} 
+                                    alt={user.name} 
+                                    height={50} 
+                                    width={50}
+                                    className="rounded-full"
+                                />
+                            </div>
+                            <div className="flex-1">
+                                <div className="font-bold">{user.email}</div>
+                                <Link className="text-sm" href="/api/auth/logout">Logout</Link>
+                            </div>
+                        </>
+                        ) : (
+                        <Link href="/api/auth/login">Login</Link>
+                    )}
                 </div>
             </div>
             <div>{children}</div>
