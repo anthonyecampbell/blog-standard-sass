d06f349a5a4ba0d83deba1392c47e4360a2976b6
Anthony Campbell <aecampbell12@gmail.com>
2023-06-10 18:03:50 -0500

    add form to pass topic and keywords to generate a blog post

--- pages/post/new.js
+++ pages/post/new.js
@@ -1,20 +1,48 @@
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { useState } from 'react';
 import { AppLayout } from '../../components/AppLayout';
 
 export default function NewPost() {
-    const handleClick = async () => {
+    const [topic, setTopic] = useState('');
+    const [keywords, setKeywords] = useState('');
+    const [postContent, setPostContent] = useState('');
+    const handleSubmit = async (e) => {
+        e.preventDefault();
         const response = await fetch('/api/generatePost', {
-            method: "POST"
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({topic, keywords}),
         });
         const json = await response.json();
-        console.log(json);
+        console.log(json.post.postContent);
+        setPostContent(json.post.postContent);
     }
     return (
       <div>
-        <h1>This is the new post page!</h1>
-        <button className="btn" onClick={handleClick}>
-            Generate
-        </button>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>
+                    <strong>
+                        Generate a blog post on the topic of:
+                    </strong>
+                </label>
+                <textarea className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm" value={topic} onChange={(e) => setTopic(e.target.value)} />
+            </div>
+            <div>
+                <label>
+                    <strong>
+                        Targeting the following keywords:
+                    </strong>
+                </label>
+                <textarea className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm" value={keywords} onChange={(e) => setKeywords(e.target.value)} />
+            </div>
+            <button type="submit" className="btn">
+                Generate
+            </button>
+        </form>
+        <div className="max-w-screen-sm p-10" dangerouslySetInnerHTML={{__html: postContent}}></div>
       </div>
     );
 }
